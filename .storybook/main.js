c505c63a4d833741d8394f7532af91ba958bce36
Armando Neto <armandoneto75@yahoo.ca>
2023-02-20 22:57:05 -0500

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,18 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react"
-}
+       stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+       addons: [
+              "@storybook/addon-links",
+              "@storybook/addon-essentials",
+              "@storybook/addon-interactions",
+              "@storybook/preset-create-react-app",
+       ],
+       framework: "@storybook/react",
+       webpackFinal: async (config) => {
+              config.module.rules.push({
+                     test: /\.css$/,
+                     use: ["style-loader", "css-loader"],
+                     include: path.resolve(__dirname, "../src/stories"),
+              });
+              return config;
+       },
+};
